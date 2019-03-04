f796d35b8d6314906f6b8d7a9a2cb6096254b3dd
jugonzal <jugonzal@gmail.com>
2019-03-04 15:56:35 -0500

    implemented special characters

--- pwdzer.js
+++ pwdzer.js
@@ -1,10 +1,21 @@
 
 // ask for a password to analyze
 
-console.log(process.argv[2])
+var password = process.argv[2];
 
 // - long one, at least 9
+if (password.length >= 9) {
+	console.log("Length: great than 9.  Awesome!")
+}
 // - at least one special character
+
+var special = "~!@#$%^&*()";
+for (i = 0; i < password.length; i++) {
+	if (special.includes(password[i])) {
+		console.log("Special Characters... found one. More Awesome!")
+	}
+}
+
 // - at least one number
 // - it can't be your email... or similar
 // - at least one uppercase and one lowercase letters
