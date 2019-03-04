d76416b2b0ec59726361108806536c2eae278222
jugonzal <jugonzal@gmail.com>
2019-03-04 16:00:31 -0500

    added support for numbers and fixed comments after length

--- pwdzer.js
+++ pwdzer.js
@@ -6,6 +6,8 @@ var password = process.argv[2];
 // - long one, at least 9
 if (password.length >= 9) {
 	console.log("Length: great than 9.  Awesome!")
+} else {
+	console.log("Not awesome... Too short")
 }
 // - at least one special character
 
@@ -17,5 +19,12 @@ for (i = 0; i < password.length; i++) {
 }
 
 // - at least one number
+var numbers = "1234567890";
+for (i = 0; i < password.length; i++) {
+	if (numbers.includes(password[i])) {
+		console.log("Numbers... found one. More Awesome!")
+	}
+}
+
 // - it can't be your email... or similar
 // - at least one uppercase and one lowercase letters
