b6448dcda8ea67bc66c15a6eca0c9a305818a294
jugonzal <jugonzal@gmail.com>
2019-03-04 15:43:49 -0500

    reading from the console

---
+++ pwdzer.js
@@ -0,0 +1,10 @@
+
+// ask for a password to analyze
+
+console.log(process.argv[2])
+
+// - long one, at least 9
+// - at least one special character
+// - at least one number
+// - it can't be your email... or similar
+// - at least one uppercase and one lowercase letters
